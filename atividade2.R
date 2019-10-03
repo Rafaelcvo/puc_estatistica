172dd9333af5bf5060b12d427e95942c798be6a8
Rafael <rafaelmoreiracvo@yahoo.com.br>
2019-10-03 08:27:59 -0300

    atividade 2

--- atividade2.R
+++ atividade2.R
@@ -2,28 +2,55 @@
 # lowbwt - Baixo peso ao nascer: 1-Low(baixo) / 0-Normal -Variavel dependente
 # https://www.datacamp.com/community/tutorials/logistic-regression-R
 
-# Importando o dataset.
+# Importando as bibliotecas
 library(readxl)
+library(Amelia)
+library(mlbench)
+library(corrplot)
+library(MASS)
+library(pscl)
+
+# Importando o dataset.
 df <- read_excel("Atividade Avaliativa 2 - birthweight.xlsx")
+
+# Verificando os dados com summary
+summary(df)
+
+# Plotando os dados
+plot(df)
+
+# Verificando a existencia de dados ausentes
+missmap(df, col = c("red", "blue"))
+
+# Criando uma matrix de dispersano
+pairs(df, col = df$lowbwt)
+
+# Calculando a correlação entre as variaveis
+correlations <- cor(df[,1:14])
+corrplot(correlations, method = "circle")
 
 # Ajustando o modelo.
 attach(df)
 ajuste <- glm(lowbwt ~ headcirumference + length + Birthweight + Gestation + smoker +
-              motherage + mnocig + mheight + mppwt + fage + fedyrs + fnocig + fheight, family = binomial)
+              motherage + mnocig + mheight + mppwt + fage + fedyrs + fnocig + fheight, 
+              family = binomial)
 
 summary(ajuste)
 
-# Selecao do melhor model.
-library(MASS)
+# Selecao do melhor modelo.
 stepAIC(ajuste, direction = "both")
 
-library(ggplot2)
-ggplot(data = df) +
-  geom_point(mapping = aes(x = lowbwt, y = Birthweight))
+# Ajustando um novo modelo
+ajuste2 <- glm(formula = lowbwt ~ Birthweight, family = binomial)
+summary(ajuste2)
 
+# Calculando os odds
+exp(cbind(coef(ajuste2), confint.default(ajuste2)))
 
+# Teste chi-quadrado
+pchisq(ajuste2$deviance, ajuste2$df.residual, lower.tail = F)
+pchisq(ajuste2$null.deviance - ajuste2$deviance,
+       ajuste2$df.null - ajuste2$df.residual, lower.tail = F)
 
-
-
-
-
+# Estimando o coeficiente de determinação
+pR2(ajuste2)
